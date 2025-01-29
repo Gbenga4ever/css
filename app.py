84e6fe2b6087f79454bc013748fc0fd6e2545b99
Gbenga Abayomi Afuye <112346679+Gbenga4ever@users.noreply.github.com>
2025-01-29 15:04:22 +0200

    Add files via upload

---
+++ app.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jan 29 12:46:26 2025
+
+@author: G. A. Afuye
+"""
+
+import streamlit as st
+
+st.title("Streamlit is amazing!")
+
+st.title("Amazing Agian")
